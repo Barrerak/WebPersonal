ed221a22e8436e4b40209207e39ba370f62578bc
Karen Barrera <barrerakari08@gmail.com>
2020-05-16 20:44:02 -0500

    Final Template Tag del proyecto

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+
+
+def contact(request):
+   return render(request, "core/contact.html")
+
