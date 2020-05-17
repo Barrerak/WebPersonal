bc6bb85f9a8b7b042ae808e9f4a2f5497d976858
Karen Barrera <barrerakari08@gmail.com>
2020-05-17 01:34:27 -0500

    Personalizando Admin

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portfolio',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('about-me/', views.about, name="about"),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
